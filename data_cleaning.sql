2a0865a8bc0d2e5da3131c30b6060570184bf880
Bopith BUN <69631241+bopith@users.noreply.github.com>
2023-03-01 11:48:00 +0900

    ✨ Improve code structure

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -5,28 +5,30 @@ Data Cleaning for Unicorn Companies Analytics Project
 */
 
 
-Select *
-From UnicornCompanies.dbo.unicorn_info
-Order by ID
+USE UnicornCompanies
 
-Select *
-From UnicornCompanies.dbo.unicorn_finance
-Order by ID
+SELECT *
+FROM UnicornCompanies.dbo.unicorn_info
+ORDER BY 1 ASC
+
+SELECT *
+FROM UnicornCompanies.dbo.unicorn_finance
+ORDER BY 1 ASC
+
 
 --------------------------------------------------------------------------------------------------------
 
 -- Check duplicate company name
 
-Select Company, Count(Company)
-From UnicornCompanies.dbo.unicorn_info
-Group by Company
-Having Count(Company) > 1
+SELECT Company, COUNT(Company)
+FROM UnicornCompanies.dbo.unicorn_info
+GROUP BY Company
+HAVING COUNT(Company) > 1
 
-Select Company, Count(Company)
-From UnicornCompanies.dbo.unicorn_finance
-Group by Company
-Having Count(Company) > 1
-
+SELECT Company, COUNT(Company)
+FROM UnicornCompanies.dbo.unicorn_finance
+GROUP BY Company
+HAVING COUNT(Company) > 1
 -- > Bolt and Fabric appear twice in both data sets. Anyway, they are in different cities / countries. 
 -- > Therefore, we will keep those data.
 
@@ -34,41 +36,52 @@ Having Count(Company) > 1
 --------------------------------------------------------------------------------------------------------
 
 -- Rename columns
+EXEC sp_rename 'dbo.unicorn_info.[Year Founded]', 'YearFounded', 'COLUMN';
+EXEC sp_rename 'dbo.unicorn_finance.[Date Joined]', 'DateJoined', 'COLUMN';
+EXEC sp_rename 'dbo.unicorn_finance.[Select Investors]', 'SelectInvestors', 'COLUMN';
 
-Exec sp_rename 'dbo.unicorn_info.[Year Founded]', 'YearFounded', 'COLUMN'
-Exec sp_rename 'dbo.unicorn_finance.[Date Joined]', 'DateJoined', 'COLUMN'
-Exec sp_rename 'dbo.unicorn_finance.[Select Investors]', 'SelectInvestors', 'COLUMN'
-
-Select *
-From UnicornCompanies.dbo.unicorn_finance
+SELECT *
+FROM UnicornCompanies.dbo.unicorn_finance
 
 
 --------------------------------------------------------------------------------------------------------
 
 -- Standardize date joined format & break out date joined into individual columns (Year, Month, Day)
 
-Alter Table UnicornCompanies.dbo.unicorn_finance
-Add DateJoinedConverted Date
-Update UnicornCompanies.dbo.unicorn_finance
-Set DateJoinedConverted = Convert(Date, DateJoined)
+ALTER TABLE UnicornCompanies.dbo.unicorn_finance
+ADD DateJoinedConverted DATE
+UPDATE UnicornCompanies.dbo.unicorn_finance
+SET DateJoinedConverted = CONVERT(DATE, DateJoined)
 
-Alter Table UnicornCompanies.dbo.unicorn_finance
-Add Year int
-Update UnicornCompanies.dbo.unicorn_finance
-Set Year = datepart(year, DateJoinedConverted)
+ALTER TABLE UnicornCompanies.dbo.unicorn_finance
+ADD Year INT
+UPDATE UnicornCompanies.dbo.unicorn_finance
+SET Year = DATEPART(YEAR, DateJoinedConverted)
 
-Alter Table UnicornCompanies.dbo.unicorn_finance
-Add Month int
-Update UnicornCompanies.dbo.unicorn_finance
-Set Month = datepart(month, DateJoinedConverted)
+ALTER TABLE UnicornCompanies.dbo.unicorn_finance
+ADD Month INT
+UPDATE UnicornCompanies.dbo.unicorn_finance
+SET Month = DATEPART(MONTH, DateJoinedConverted)
 
-Alter Table UnicornCompanies.dbo.unicorn_finance
-Add Day int
-Update UnicornCompanies.dbo.unicorn_finance
-Set Day = datepart(day, DateJoinedConverted)
+ALTER TABLE UnicornCompanies.dbo.unicorn_finance
+ADD Day INT
+UPDATE UnicornCompanies.dbo.unicorn_finance
+SET Day = DATEPART(DAY, DateJoinedConverted)
 
-Select *
-From UnicornCompanies.dbo.unicorn_finance
+SELECT *
+FROM UnicornCompanies.dbo.unicorn_finance
+
+
+--------------------------------------------------------------------------------------------------------
+
+-- Drop rows where Funding column contain 0 or Unknown
+
+DELETE FROM UnicornCompanies.dbo.unicorn_finance 
+WHERE Funding IN ('$0M', 'Unknown')
+
+SELECT DISTINCT Funding
+FROM UnicornCompanies.dbo.unicorn_finance
+ORDER BY Funding DESC
 
 
 --------------------------------------------------------------------------------------------------------
@@ -77,30 +90,30 @@ From UnicornCompanies.dbo.unicorn_finance
 
 -- "Valuation" and "Funding" columns
 
-Update UnicornCompanies.dbo.unicorn_finance
-Set Valuation = Right(Valuation, LEN(Valuation) - 1)
+UPDATE UnicornCompanies.dbo.unicorn_finance
+SET Valuation = RIGHT(Valuation, LEN(Valuation) - 1)
 
-Update UnicornCompanies.dbo.unicorn_finance
-Set Valuation = Replace(Replace(Valuation, 'B','000000000'), 'M', '000000')
+UPDATE UnicornCompanies.dbo.unicorn_finance
+SET Valuation = REPLACE(REPLACE(Valuation, 'B','000000000'), 'M', '000000')
 
-Update UnicornCompanies.dbo.unicorn_finance
-Set Funding = Right(Funding, LEN(Funding) - 1)
+UPDATE UnicornCompanies.dbo.unicorn_finance
+SET Funding = RIGHT(Funding, LEN(Funding) - 1)
 
-Update UnicornCompanies.dbo.unicorn_finance
-Set Funding = Replace(Replace(Funding, 'B','000000000'), 'M', '000000')
+UPDATE UnicornCompanies.dbo.unicorn_finance
+SET Funding = REPLACE(REPLACE(Funding, 'B','000000000'), 'M', '000000')
 
-Select *
-From UnicornCompanies.dbo.unicorn_finance
+SELECT *
+FROM UnicornCompanies.dbo.unicorn_finance
 
 
 --------------------------------------------------------------------------------------------------------
 
 -- Delete Unused Columns
 
-Alter Table UnicornCompanies.dbo.unicorn_finance
-Drop Column DateJoined
+ALTER TABLE UnicornCompanies.dbo.unicorn_finance
+DROP COLUMN DateJoined
 
-Exec sp_rename 'dbo.unicorn_finance.DateJoinedConverted', 'DateJoined', 'COLUMN'
+EXEC sp_rename 'dbo.unicorn_finance.DateJoinedConverted', 'DateJoined', 'COLUMN'
 
-Select *
-From UnicornCompanies.dbo.unicorn_finance
+SELECT *
+FROM UnicornCompanies.dbo.unicorn_finance
